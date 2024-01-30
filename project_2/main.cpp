8c504b33430f3932be1d650092ad065529b9e175
Leison Gao <leisongao2005@g.ucla.edu>
2024-01-29 23:31:20 -0800

    asdf

--- project_2/main.cpp
+++ project_2/main.cpp
@@ -10,96 +10,45 @@
 #include "Map.hpp"
 using namespace std;
 
-//bool merge(const Map& m1, const Map& m2, Map& result) {
-////    Map tempMap = new Map(m1);
-//    while (result.size() > 0) {
-//        KeyType key;
-//        ValueType value;
-//        result.get(0, key, value);
-//        result.erase(key);
-//    }
-//
-//    
-//    cout << "empty map" << endl;
-//    result.dump();
-//    cout << result.size() << endl;
-//    
-//    for (int i = 0; i < m1.size(); i ++) {
-//        KeyType key;
-//        ValueType value;
-//        m1.get(i, key, value);
-//        result.insert(key, value);
-//    }
-////    cout << "copy of first map" << endl;
-////    result.dump();
-//    
-//    bool dupes = false;
-//    for(int i = 0; i < m2.size(); i ++) {
-//        KeyType key;
-//        ValueType value;
-//        m2.get(i, key, value);
-////        cout << "trying to insert " << key << " " << value << endl;
-//        ValueType tempValue;
-//        result.get(key, tempValue);
-////        cout << "temp map contains" <<  key << " " << tempValue << endl;
-//        
-////        tempMap.dump();
-//        if (!result.contains(key)) { // map does not contain value
-//            result.insert(key, value);
-//        }
-//        else if (tempValue != value) { // cannot insert
-////            cout << "cannot insert " << key << endl;
-////            tempMap.dump();
-//            result.erase(key);
-//            dupes = true;
-//        }
-//        else
-//            result.insert(key, value);
-//        
-//    }
-////    tempMap.dump();
-////    tempMap.insert("Johnny", 123);
-////    cout << "temp map:" << endl;
-////    tempMap.dump();
-////    result = tempMap;
-////    result.dump();
-//    if (dupes)
-//        return false;
-//    return true;
-//}
-
-
 bool merge(const Map& m1, const Map& m2, Map& result) {
-    Map* temp = new Map(m1);
+    while (result.size() > 0) {
+        KeyType key;
+        ValueType value;
+        result.get(0, key, value);
+        result.erase(key);
+    }
+    
+    for (int i = 0; i < m1.size(); i ++) {
+        KeyType key;
+        ValueType value;
+        m1.get(i, key, value);
+        result.insert(key, value);
+    }
+    
     bool dupes = false;
-    for(int i = 0; i < m2.size(); i++) {
+    for(int i = 0; i < m2.size(); i ++) {
         KeyType key;
         ValueType value;
         m2.get(i, key, value);
-//        cout << "trying to insert " << key << " " << value << endl;
         ValueType tempValue;
-        temp->get(key, tempValue);
-//        cout << "temp map contains" <<  key << " " << tempValue << endl;
-
-//        tempMap.dump();
-        if (!temp->contains(key)) { // map does not contain value
-            temp->insert(key, value);
+        result.get(key, tempValue);
+        if (!result.contains(key)) { // map does not contain value
+            result.insert(key, value);
         }
-        else if (tempValue != value) { // cannot insert
-//            cout << "cannot insert " << key << endl;
-//            tempMap.dump();
-            temp->erase(key);
+        else if (tempValue != value) {
+            result.erase(key);
             dupes = true;
         }
         else
-            temp->insert(key, value);
+            result.insert(key, value);
         
     }
-    result = *temp;
+    
     if (dupes)
         return false;
     return true;
 }
+
 void reassign(const Map& m, Map& result) {
     Map tempMap;
     KeyType key;
@@ -140,11 +89,49 @@ int main() {
 //    reassign(m1, m3);
 //    m3.dump();
 //    m1.dump();
-    if (merge(m1, m2, m3))
-        cout << "Successfully merged!" << endl;
-    else
-        cout << "Did not merge" << endl;
+//    if (merge(m1, m2, m3))
+//        cout << "Successfully merged!" << endl;
+//    else
+//        cout << "Did not merge" << endl;
+//    
+//    cout << m3.size() << endl;
+//    m3.dump();
     
-    cout << m3.size() << endl;
-    m3.dump();
+    while (m3.size() > 0) {
+        KeyType key;
+        ValueType value;
+        m3.get(0, key, value);
+        m3.erase(key);
+    }
+    
+    for (int i = 0; i < m1.size(); i ++) {
+        KeyType key;
+        ValueType value;
+        m1.get(i, key, value);
+        m3.insert(key, value);
+    }
+    
+    bool dupes = false;
+    for(int i = 0; i < m2.size(); i ++) {
+        KeyType key;
+        ValueType value;
+        m2.get(i, key, value);
+        ValueType tempValue;
+        m3.get(key, tempValue);
+        if (!m3.contains(key)) { // map does not contain value
+            m3.insert(key, value);
+        }
+        else if (tempValue != value) {
+            m3.erase(key);
+            dupes = true;
+        }
+        else
+            m3.insert(key, value);
+        
+    }
+    
+    if (dupes)
+        cout << "Duplicates found!" << endl;
+    else
+        cout << "All Clear!" << endl;
 }
